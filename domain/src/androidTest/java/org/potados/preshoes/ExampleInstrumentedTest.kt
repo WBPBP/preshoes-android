f34fab9d51396ee1900f7e7fc332d8373e1d4046
potados99 <potados99@gmail.com>
2020-02-09 17:34:10 +0900

    domain 모듈 추가

--- domain/src/androidTest/java/org/potados/preshoes/ExampleInstrumentedTest.kt
+++ domain/src/androidTest/java/org/potados/preshoes/ExampleInstrumentedTest.kt
@@ -17,5 +17,27 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-include ':app', ':domain'
-rootProject.name='preshoes'
+package org.potados.preshoes
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.potados.preshoes", appContext.packageName)
+    }
+}
